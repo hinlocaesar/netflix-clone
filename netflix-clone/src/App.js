36f5fd955a59babb11d066a1c872d866c648eda3
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-18 19:42:47 +0800

    [ER]Implementation of Banner base

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
+
       <Row
         title="NETFLX ORIGINALS"
         fetchUrl={requests.fetchDiscoverNetflixOriginal}
+        isLargeRow
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
---
+++ netflix-clone/src/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Banner(props) {
+  return (
+    <header>
+      {" "}
+      {/*<<<---Give this header a background image */}
+      {/*Title */}
+      {/*div >2 button */}
+      {/*description */}
+    </header>
+  );
+}
+
+export default Banner;
