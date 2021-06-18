22743009e476a3b3e246024b134274394e036ca0
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-18 13:31:51 +0800

    [ER]created files for axios

---
+++ netflix-clone/src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "ca39b88b8ea32be7a47a732f661552a4";
+
+const requests={
+fetchTrending:`/movie/all/week?api_key=${API_KEY}&language=en-US`,
+fetchDiscoverNetflixOriginal:`/discover/tv?api_key=${API_KEY}`,
+fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+fetchActionMovies:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+fetchComedyMovies:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+fetchHorrorMovies:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+fetchRomanceMovies:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+fetchDocumentaries:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+}
+
+export default requests;
