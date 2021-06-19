b35937ec4745bab14c69b073137e78a0e4861ab8
Caesar <caesar.hinlo.tech@gmail.com>
2021-06-19 12:24:25 +0800

    [ER]Styling for nav bar positon fixed event implementation

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -16,15 +19,39 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+      console.log("HINLO HANDLE 1");
+    } else {
+      movieTrailer(movie?.name || "") // npm package to find movie
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+      console.log("HINLO HANDLE 1");
+    }
+  };
+
   return (
     <div className="row">
       {/*title*/}
       <h2>{title}</h2>
-
       <div className="row__posters">
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -33,6 +60,8 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {console.log("[HINLO][DEBUG]", trailerUrl.trai)}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
